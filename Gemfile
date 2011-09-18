a57510e9adb73879e504a14d0a292ff20db3a3c9
Jeff Powell <powellj@mac.com>
2011-09-18 12:24:57 -0600

    Added Users model and annotate models

--- Gemfile
+++ Gemfile
@@ -5,6 +5,11 @@ gem 'sqlite3', '1.3.3'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
+  gem 'pry'
+  gem 'pry-doc'
+  gem 'hirb' 
+  gem 'awesome_print' 
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,14 @@ SampleApp::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # Use Pry instead of IRB
+  silence_warnings do
+    begin
+      require 'pry'
+      IRB = Pry
+    rescue LoadError
+    end
+  end
 end
 
