526d5f0aa6adf61a2b982616fc9fc7423faf74ba
Jeff Powell <powellj@mac.com>
2011-09-18 18:45:54 -0600

    Finished first cut of the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,16 @@
 #  email      :string(255)
 #  created_at :datetime
 #  updated_at :datetime
-#
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+
+ email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
